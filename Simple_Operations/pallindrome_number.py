8197dacfd8864826c8e60ad3cede0ecef1321d1e
Deepak Thorat <thoratdeepak63@gmail.com>
2026-08-22 22:06:20 +0530

    Explanation for how the loop works with example

--- Simple_Operations/pallindrome_number.py
+++ Simple_Operations/pallindrome_number.py
@@ -9,4 +9,15 @@ if (n == result):
     print("Pallindrome")
 else:
     print("Not a Pallindrome")
-        
+#how the loop works for 123
+#last_digit = 123 % 10 = 3
+#result = (0 * 10) + 3 = 3
+#num = 123 // 10 = 12
+
+#last_digit = 12 % 10 = 2
+#result = (3 * 10) + 2 = 32
+#num = 12 // 10 = 1
+
+#last_digit = 1 % 10 = 1
+#result = (32 * 10) + 1 = 321
+#num = 1 // 10 = 0
