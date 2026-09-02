10b4893d307b1b62e9f95eb5774e6b88f2bd8797
Deepak Thorat <thoratdeepak63@gmail.com>
2026-09-02 20:20:04 +0530

    Update the TC and SC part

--- List_array/Max_consecutives.py
+++ List_array/Max_consecutives.py
@@ -1,3 +1,17 @@
+'''
+Leetcode 485 question MAX CONSECUTIVE ONEs
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+'''
+
 nums = [1, 1,1,1,0,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,0,1,1,1]
 max_count = 0
 count = 0
@@ -10,3 +24,6 @@ for i in range(0, n):
         count = 0
 max_count = max(max_count, count)
 print(max_count)
+
+# T.C =O(n)
+# S.C = O(1)
