9f8692d3183d8ce350030d06c1f7b93ac08834e0
Deepak Thorat <thoratdeepak63@gmail.com>
2026-09-02 20:18:42 +0530

    Add the file max_consective to the List_array folder

---
+++ List_array/Max_consecutives.py
@@ -0,0 +1,12 @@
+nums = [1, 1,1,1,0,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,0,1,1,1]
+max_count = 0
+count = 0
+n = len(nums)
+for i in range(0, n):
+    if(nums[i] == 1):
+        count += 1
+    else:
+        max_count = max(max_count, count)
+        count = 0
+max_count = max(max_count, count)
+print(max_count)
